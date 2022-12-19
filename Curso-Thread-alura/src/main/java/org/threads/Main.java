19136b73289335efc0312839c09d4a1c332a8d61
Lucas Santos <lucas.parkl@outlook.com>
2022-12-19 19:39:23 -0300

    WIP: Threads em Java 1: - Através das Threads podemos executar tarefas em paralelo; - Uma classe que implementa a interface Runnable define a tarefa que o Thread executará; - O construtor da classe Thread recebe esse Runnable; - Devemos inicializar uma Thread explicitamente através do método start(); - Através do Thread.sleep(millis) podemos mandar uma thread dormir.

---
+++ Curso-Thread-alura/src/main/java/org/threads/Main.java
@@ -0,0 +1,11 @@
+package org.threads;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+
+        System.out.println("Thread da classe main");
+
+        Thread.sleep(60000);
+
+    }
+}
